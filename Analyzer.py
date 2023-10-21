708b743025f9b4145719f1b78be274ebb6be9eb8
An3s079 <An3sferreira@gmail.com>
2023-10-21 10:10:44 -1000

    attempt 1

--- Analyzer.py
+++ Analyzer.py
@@ -12,19 +12,20 @@ openai.api_key = apiFile.read()
 apiFile.close()
 
 for filename in os.listdir("News Articles"):
-    file = open("News Articles\\" + filename, "r+")
+    file = open("News Articles\\" + filename, "r+", encoding="utf8")
+    Data = file.read()
     response = openai.ChatCompletion.create(
-        model="gpt-4.0",
+        model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You are analyzing news articles to determine if there is a difference in how harmful they portray a crime to be, "+
             "and how much malicious intent they attribute to the crime. Please rate their portrayed harmfullness and portrayed malicious intent from 1-10. Remember, please rate the articles based on "+
-            "how harmful and malicious they portray the crime to be, not how malicious you believe the crime to be. Please Lay out your responses like so: "+
+            "how harmful and malicious they portray the crime to be, not how harmful or malicious you believe the crime to be. Please Lay out your responses like so: "+
             "\"[<harmfullness rating>/10, <maliciousness rating>/10] <insert new line> <reason for harmfullness rating> (<harmfullness rating>/10),  <reason for maliciousness rating> (<maliciousness rating>/10)"},
-            {"role": "user", "content": file.read()}
+            {"role": "user", "content": Data}
         ]
     )
-    file.seek(0, 0)
-    file.write(response["choices"][0]["text"] + "\n")
+    file.seek(0,0)
+    file.write(str(response["choices"][0]["message"]["content"]) + "\n" + "\n" + Data)
     file.close()
 
     
